f974f454f2d7e8a4123d55ffb7f23118d3022827
chediak <shammcity00@gmail.com>
2020-10-29 14:06:37 -0300

    post

--- server/routes/owner.js
+++ server/routes/owner.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 const Owner = require('../models/owner');
+const upload = require('../middlewares/upload-photo');
 
 // Post Api
-router.post('/owners', async (req, res) => {
+router.post('/owners', upload.single("photo"), async (req, res) => {
     try {
         let owner = new Owner();
         owner.name = req.body.name;
         owner.about = req.body.about;
+        owner.photo = req.file.location;
         await owner.save();
 
         res.json({
@@ -14,6 +16,7 @@ router.post('/owners', async (req, res) => {
             message: "successfully created!"
         });
     } catch (err) {
+        console.log(err);
         res.json({
             success: false,
             message: err.message
