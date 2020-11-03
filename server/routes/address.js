49359b7d65185c5a22810c6947f9815f8ff13841
chediak <shammcity00@gmail.com>
2020-11-03 12:18:06 -0300

    html

--- server/routes/address.js
+++ server/routes/address.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Address = require('../models/address');
 const verifyToken = require('../middlewares/verify-token');
-const address = require('../models/address');
+const axios = require('axios');
 
 router.post("/addresses", verifyToken, async (req, res) =>{
     try {
@@ -45,5 +45,18 @@ router.get('/addresses', verifyToken, async (req, res) => {
         });
     }
 })
+
+router.get('/countries', async (req, res) => {
+    try {
+        let response = await axios.get('https://restcountries.eu/rest/v2/all');
+
+        res.json(response.data);
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
 
 module.exports = router;
